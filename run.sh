ee902f3725b58cefbc7069f29f0da307c1f67a44
diamondhands0 <diamondhands@bitcloutdev.com>
2021-04-07 21:23:28 -0700

    Beef up README

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 set -e
 
 if ! [ -x "$(command -v docker-compose)" ]; then
-    echo "Please install docker: https://docs.docker.com/get-docker/"
+    echo "Please install docker and docker-compose: https://docs.docker.com/get-docker/"
     exit 1
 fi
 
-docker-compose pull
+docker-compose pull || ( echo -e "\n\n\nTry running ./run.sh with sudo" && exit 1 )
 docker-compose up
