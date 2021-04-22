4613b8d3c518f9b4ad648dc0195f4c6d7732ff1b
diamondhands0 <diamondhands@bitcloutdev.com>
2021-04-21 23:58:24 -0700

    Flags mostly working

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+DOCKER_COMPOSE_FILENAME=docker-compose.dev.yml
+
 set -e
 
 if ! [ -x "$(command -v docker-compose)" ]; then
@@ -6,5 +9,5 @@ if ! [ -x "$(command -v docker-compose)" ]; then
     exit 1
 fi
 
-docker-compose pull || ( echo -e "\n\n\nTry running ./run.sh with sudo" && exit 1 )
-docker-compose up
+docker-compose -f ${DOCKER_COMPOSE_FILENAME} pull || ( echo -e "\n\n\nTry running ./run.sh with sudo" && exit 1 )
+docker-compose -f ${DOCKER_COMPOSE_FILENAME} up
