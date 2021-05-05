77cf44dc75f514c4db313e6d6422212de8c07373
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2021-05-05 15:33:06 -0400

    Run run.sh in any dir (#20)

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ DOCKER_COMPOSE_FILENAME=docker-compose.dev.yml
 
 set -e
 
+RUN_ROOT="$(dirname "$0")"
+
+pushd $RUN_ROOT
+
 if ! [ -x "$(command -v docker-compose)" ]; then
     echo "Please install docker and docker-compose: https://docs.docker.com/get-docker/"
     exit 1
@@ -11,3 +15,5 @@ fi
 
 docker-compose -f ${DOCKER_COMPOSE_FILENAME} pull || ( echo -e "\n\n\nTry running ./run.sh with sudo" && exit 1 )
 docker-compose -f ${DOCKER_COMPOSE_FILENAME} up "$@"
+
+popd
